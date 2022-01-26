d6a9fd25a237b4f2d68337abdb73dc8a9766c00e
MOrowo <athipat.b@gmail.com>
2022-01-26 12:55:28 +0700

    edit: update config + prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        "raspberry": "#d90077",
-        "deepblue": "#0052ff",
-        "nice_gray": "#e5e7eb"
-      }
+        raspberry: '#d90077',
+        deepblue: '#0052ff',
+        nice_gray: '#e5e7eb',
+      },
+    },
+    borderRadius: {
+      half: '50%',
     },
   },
   plugins: [],
-}
+};
