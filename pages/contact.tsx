0d99c44c2c08e70cae12dc3c5d950cff2da39bd6
MOrowo <athipat.b@gmail.com>
2022-01-29 00:06:17 +0700

    Add: Initial contact form

---
+++ pages/contact.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+
+import Head from 'next/head';
+import Contactform from '../src/components/Contactform';
+import Footer from '../src/components/Footer';
+import Navbar from '../src/components/Navbar';
+
+const Contact: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>MOrowo</title>
+        <meta name="description" content="MOrowo | Contact" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <Contactform />
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
