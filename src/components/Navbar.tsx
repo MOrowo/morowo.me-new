5a799591f0a7dbce43cef0d8a8a5bb3fa1ad4103
MOrowo <athipat.b@gmail.com>
2022-01-29 00:08:46 +0700

    add: navbar change on ssr

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { scrollToAbout } from './Nav/default';
 
 const Navbar = () => {
+  const router = useRouter();
+  const active =
+    'py-4 px-2 text-deepblue border-b-4 border-deepblue font-semibold';
+  const inactive =
+    'py-4 px-2 text-gray-500 font-semibold hover:text-deepblue transition duration-300';
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = useEffect(() => {
@@ -19,30 +27,39 @@ const Navbar = () => {
             <div className="flex space-x-14">
               {/* Primary Navbar items */}
               <div className="hidden md:flex items-center space-x-20">
-                <a
-                  href=""
-                  className="py-4 px-2 text-deepblue border-b-4 border-deepblue font-semibold "
-                >
-                  Home
-                </a>
-                <a
-                  className="py-4 px-2 text-gray-500 font-semibold hover:text-deepblue transition duration-300"
-                  onClick={scrollToAbout}
-                >
-                  About
-                </a>
-                <a
-                  href=""
-                  className="py-4 px-2 text-gray-500 font-semibold hover:text-deepblue transition duration-300"
-                >
-                  Projects
-                </a>
-                <a
-                  href=""
-                  className="py-4 px-2 text-gray-500 font-semibold hover:text-deepblue transition duration-300"
-                >
-                  Contact Me
-                </a>
+                <Link href="/">
+                  <a
+                    id="index"
+                    className={router.pathname == '/' ? active : inactive}
+                  >
+                    Home
+                  </a>
+                </Link>
+                <Link href="/" scroll={false}>
+                  <a className={inactive} onClick={scrollToAbout}>
+                    About
+                  </a>
+                </Link>
+                <Link href="/projects">
+                  <a
+                    id="projects"
+                    className={
+                      router.pathname == '/projects' ? active : inactive
+                    }
+                  >
+                    Projects
+                  </a>
+                </Link>
+                <Link href="/contact">
+                  <a
+                    id="contact"
+                    className={
+                      router.pathname == '/contact' ? active : inactive
+                    }
+                  >
+                    Contact Me
+                  </a>
+                </Link>
               </div>
             </div>
             {/* Mobile menu button */}
