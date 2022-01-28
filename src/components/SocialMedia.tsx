232552fecae9efdc22f4266b68ca3b2cb7e1ef29
MOrowo <athipat.b@gmail.com>
2022-01-29 00:09:31 +0700

    edit: rearrange icon order

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const SocialMedia = () => {
   return (
-    <div className="flex gap-12">
+    <div className="flex gap-14">
+      <a href="https://github.com/morowo">
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" />
+      </a>
       <a href="https://instagram.com/morowo__">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" />
       </a>
       <a href="https://twitter.com/morowo6">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png" />
-      </a>
-      <a href="https://github.com/morowo">
-        <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" />
       </a>
     </div>
   );
