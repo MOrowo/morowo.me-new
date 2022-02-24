6654d6c45081c2b24382d1214b682f51bad963b8
MOrowo <athipat.b@gmail.com>
2022-02-25 00:02:23 +0700

    edit: using built-in fetch instead of axios

--- pages/shortener/[id].tsx
+++ pages/shortener/[id].tsx
@@ -1,30 +1,34 @@
 import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
-const axios = require('axios').default;
 
-const test: NextPage = () => {
+const Id: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
   useEffect(() => {
     if (id != null)
-      axios.get(`https://morowo.me/api/${id}`).then((res: any) => {
-        let result1 = document.getElementById('result1');
-        if (result1) {
-          if (res.data.status === 200) {
-            const redirect_url = res.data.res;
-            result1.innerHTML = 'redirecting to ' + redirect_url;
-            window.location = redirect_url;
+      fetch(`/api/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((res: any) => res.json())
+        .then((data: any) => {
+          let result1 = document.getElementById('result1');
+          if (result1) {
+            if (data.status === 200) {
+              const redirect_url = data.res;
+              result1.innerHTML = 'redirecting to ' + redirect_url;
+              window.location = redirect_url;
+            }
+            if (data.status === 404) {
+              result1.classList.add('text-4xl');
+              result1.innerHTML = 'Invalid link!';
+            }
           }
-          if (res.data.status === 404) {
-            result1.classList.add('text-4xl');
-            result1.innerHTML = 'Invalid link!';
-          }
-        }
-      });
+        });
   }, [id]);
   return <div id="result1"></div>;
 };
 
-export default test;
+export default Id;
