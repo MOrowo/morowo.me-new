ebe3f620ad48fcb8851594e6c6c12360195a04c6
MOrowo <athipat.b@gmail.com>
2022-05-30 23:21:45 +0700

    add: apply _blank to links

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 const SocialMedia = () => {
   return (
     <div className="flex gap-14">
-      <a href="https://github.com/morowo">
+      <a target="_blank" href="https://github.com/morowo">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" />
       </a>
-      <a href="https://instagram.com/realmorowo">
+      <a target="_blank" href="https://instagram.com/realmorowo">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" />
       </a>
-      <a href="https://twitter.com/morowo6">
+      <a target="_blank" href="https://twitter.com/morowo6">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png" />
       </a>
     </div>
