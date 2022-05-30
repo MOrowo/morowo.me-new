5c643344093e5905c887858e7980864b0e5294ed
MOrowo <athipat.b@gmail.com>
2022-05-30 23:21:22 +0700

    edit: change text on production

---
+++ pages/projects.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+
+import Head from 'next/head';
+import Footer from '../src/components/Footer';
+import Navbar from '../src/components/Navbar';
+import Projectspage from '../src/components/Projectspage';
+
+const Projects: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>MOrowo</title>
+        <meta name="description" content="MOrowo | Contact" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      {/* add project I've made here */}
+      <Projectspage />
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
