203189b76cc8262a96b722b9ce2ed7b2569b413e
MOrowo <athipat.b@gmail.com>
2022-05-30 23:22:19 +0700

    add: tracking projects

---
+++ src/components/Projectspage.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/Home.module.css';
+
+const Projectspage = () => {
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <div className="flex w-full flex-col gap-20">
+            <div id="parent-text">
+              <p className="text-3xl text-deepblue font-bold">Coming soon...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projectspage;
