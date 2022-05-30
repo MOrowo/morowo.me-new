6d9a2fcf45800158189eea6924645c32c233bc02
MOrowo <athipat.b@gmail.com>
2022-05-30 23:27:51 +0700

    edit: fix failed build and prettiered

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -3,13 +3,25 @@ import React from 'react';
 const SocialMedia = () => {
   return (
     <div className="flex gap-14">
-      <a target="_blank" href="https://github.com/morowo">
+      <a
+        target="_blank"
+        href="https://github.com/morowo"
+        rel="noopener noreferrer"
+      >
         <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" />
       </a>
-      <a target="_blank" href="https://instagram.com/realmorowo">
+      <a
+        target="_blank"
+        href="https://instagram.com/realmorowo"
+        rel="noopener noreferrer"
+      >
         <img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" />
       </a>
-      <a target="_blank" href="https://twitter.com/morowo6">
+      <a
+        target="_blank"
+        href="https://twitter.com/morowo6"
+        rel="noopener noreferrer"
+      >
         <img src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png" />
       </a>
     </div>
